9f7c989b808c0c38239bc66386960dc9b3fd06cd
nubelsondev <nubelsondev@hotmail.com>
2019-12-19 16:09:16 +0000

    feat: DataBase Connection

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
+// Definir routes como roteador (Router) da aplicação
 const routes = require("express").Router();
 
+// Criar rota inicial para teste do app
 routes.get("/", (req, res) => res.json({ hello: "World!" }));
 
+// Exportar rotas
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
+require("dotenv/config"); // Importar dotenv para acessar variáveis ambientes
 
-const app = express();
-app.use(express.json());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
+const dbUrl = process.env.MONGO_URL;
+const app = express(); // Iniciando aplicação
+app.use(express.json()); // Permitir leitura de dados em json
 
-app.use(routes);
+// Iniciar conexão com o Banco de dados
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
+app.use(routes); // Acessar arquivo de rotas
+
+// Configurar porta a ser ouvida
 app.listen(port, () => console.log(`Port ${port} is open!`));
