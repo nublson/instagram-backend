8482406b397e7a069bcddc969f01a17355b4f130
nubelsondev <nubelsondev@hotmail.com>
2019-12-20 09:07:30 +0000

    feat: add a static url to access images

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const routes = require("./routes");
 require("dotenv/config"); // Importar dotenv para acessar variáveis ambientes
 
@@ -7,6 +8,12 @@ const port = process.env.PORT || 3333;
 const dbUrl = process.env.MONGO_URL;
 const app = express(); // Iniciando aplicação
 app.use(express.json()); // Permitir leitura de dados em json
+
+// Criar rota estática para visualizar imagens
+app.use(
+    "/files",
+    express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
 
 // Iniciar conexão com o Banco de dados
 mongoose.connect(dbUrl, {
